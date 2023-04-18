668da03f86938b5f6e6b6a46c70da8a43108e13a
Ian Helbig <116044614+Helburger22@users.noreply.github.com>
2023-04-18 11:31:45 -0400

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        if (Input.anyKey && !isGameActive)
         {
             StartGame();
         }
